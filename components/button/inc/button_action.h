d04693ea8b1351cbb5681801a431088c68ceae32
Matej Brecic <brecic@byte-lab.com>
2021-11-22 17:04:07 +0100

    Additional project template setup

--- components/button/inc/button_action.h
+++ components/button/inc/button_action.h
@@ -1,45 +1,37 @@
 /**
- * @file main.c
+ * @file button_action.h
  * @author brex97 (github)
- * @brief 
  * @date 2021-11-22
- *
+ * 
  */
 
+#ifndef __BUTTON_ACTION_H__
+#define __BUTTON_ACTION_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 //--------------------------------- INCLUDES ----------------------------------
-#include <stdio.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
 
 //---------------------------------- MACROS -----------------------------------
+
 //-------------------------------- DATA TYPES ---------------------------------
+
 //---------------------- PRIVATE FUNCTION PROTOTYPES --------------------------
+
 //------------------------- STATIC DATA & CONSTANTS ---------------------------
+
 //------------------------------- GLOBAL DATA ---------------------------------
+
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
-void app_main(void)
-{
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    for (;;)
-    {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
 
 //---------------------------- PRIVATE FUNCTIONS ------------------------------
 
 //---------------------------- INTERRUPT HANDLERS -----------------------------
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __BUTTON_ACTION_H__
